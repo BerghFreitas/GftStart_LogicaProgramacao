c36dd05fb55aca9e3c1ed9d9eccad6b53a3dd95d
Aldembergue <berg2843@gmail.com>
2024-10-11 17:00:42 -0300

    array e matriz
    manipulando array e matriz

---
+++ vetores_matrizes/matrizes.js
@@ -0,0 +1,5 @@
+let timePokemon = [
+    ['pikachu', 'm', 3],
+    ['charmander', 'f', 5]
+    ]
+    console.log(timePokemon[0][1]) // significa que eu pegue a primeira linha [0] e a segunda coluna [1]
