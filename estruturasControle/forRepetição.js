f4c6610b2e0c8b057f2c1244a69fc48857e0de5c
Aldembergue <berg2843@gmail.com>
2024-10-15 14:05:14 -0300

    estruturas
    exemplos de cod, de laços

---
+++ estruturasControle/forRepetição.js
@@ -0,0 +1,5 @@
+let num = 0
+
+for(let num = 1 ; num <= 10; num ++){ //INICIALIZAÇÃO, CONDIÇÃO E INCREMENTO TUDO EM UMA LINHA
+    console.log(num)
+}
---
+++ estruturasControle/whileRepeticao.js
@@ -0,0 +1,8 @@
+ let num = 0
+
+ while (num < 10){ //AQUI SE COLOCA APENAS A CONDIÇAO
+    num ++ // POREM TEM QUE HAVER INCREMENTO SE NAO O LAÇO SE TORNA INFINITO
+    console.log(num)
+    
+
+ }
