81179c4b935b7bd40288b4144a6d203cebb059df
bergh freitas <berg2843@gmail.com>
2024-10-15 16:43:49 -0300

    desafio felipe dio
    desafio do heroi

---
+++ desafio felipe dio/desafioHeroi.js
@@ -0,0 +1,18 @@
+let nickName = "Ruma de Bosta"
+const mensagem = "o Herói de nome " + nickName + " está no nível de"
+let xp = 100001 
+    if (xp <= 1000){
+        console.log(`${mensagem} Ferro!`)
+    } else if (xp <= 2000){
+        console.log(`${mensagem} Bronze!`)
+    } else if (xp <= 5000){
+        console.log(`${mensagem} prata!`)
+    } else if (xp <= 7000){
+        console.log(`${mensagem} Ouro!`)
+    } else if (xp <= 9000){
+        console.log(`${mensagem} Platina!`)
+    } else if (xp <= 10000){
+        console.log(`${mensagem} Ascendente!`)
+    } if (xp >= 10001){
+        console.log(`${mensagem} Imortal!`)
+    } 
